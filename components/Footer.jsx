a027e622e72c44ae29451ad6578f962ff23bf6e6
milleseseau <leojimlavi@gmail.com>
2022-02-07 23:29:16 -0500

    adding new design features and correcting design on the page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -38,15 +38,15 @@ const Footer = () => {
 						</a>
 					</div>
 					<p className={styles.copyright}>
-						Copyright © 2022 Ninjaki Bar. All rights reserved.
+						Copyright © 2022 Ninjamaki Bar. All rights reserved.
 					</p>
 				</div>
 			</div>
 			<div className={styles.item}>
 				<iframe
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d184551.80858407923!2d-79.51814067414323!3d43.71840381077066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C%20Ontario%2C%20Canad%C3%A1!5e0!3m2!1ses-419!2spe!4v1644041018642!5m2!1ses-419!2spe"
-					width="632"
-					height="550"
+					width="532"
+					height="450"
 					style={{ border: "0" }}
 					allowFullScreen=""
 					loading="lazy"
