db1e621050236278aed959e3f2823117d2d8817f
milleseseau <leojimlavi@gmail.com>
2022-02-08 23:16:09 -0500

    Responsive design first page (tablet

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false);
+
 	return (
 		<div className={styles.container}>
 			<Link href="/" passHref>
@@ -24,6 +27,11 @@ const Navbar = () => {
 				</ul>
 			</div>
 			<div className={styles.itemRight}>
+				<div className={styles.hamburger} onClick={() => setOpen(!open)}>
+					<div className={styles.line} />
+					<div className={styles.line} />
+					<div className={styles.line} />
+				</div>
 				<div className={styles.cart}>
 					<div className={styles.texts}>
 						<div className={styles.text}>931 281 232</div>
@@ -35,13 +43,26 @@ const Navbar = () => {
 							style={{ fontSize: "40px" }}
 						/>
 					</div>
-					<ShoppingCartIcon
-						className={styles.svgCart}
-						style={{ fontSize: "30px" }}
-					/>
-					<div className={styles.counter}>2</div>
+					<div className={styles.cartWrapper}>
+						<ShoppingCartIcon
+							className={styles.svgCart}
+							style={{ fontSize: "30px" }}
+						/>
+						<div className={styles.counter}>2</div>
+					</div>
 				</div>
 			</div>
+			<ul
+				onClick={() => setOpen(false)}
+				className={styles.menu}
+				style={{ right: open ? "0px" : "-50vw" }}
+			>
+				<li className={styles.menuItem}>Home</li>
+				<li className={styles.menuItem}>About</li>
+				<li className={styles.menuItem}>Menu</li>
+				<li className={styles.menuItem}>Contact</li>
+				<li className={styles.menuItem}>About Us</li>
+			</ul>
 		</div>
 	);
 };
