b26eba6b3dcb830790986f18aae45b8a592b2f52
lavitz <leojimlavi@gmail.com>
2022-07-16 00:04:13 -0500

    fixing error 500: await dbConnect() async function

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -2,27 +2,27 @@ import dbConnect from "../../../utils/mongo";
 import Order from "../../../models/Order";
 
 const handler = async (req, res) => {
-	const { method } = req;
+  const { method } = req;
 
-	dbConnect() -> await dbConnect(); 
+  await dbConnect();
 
-	if (method === "GET") {
-		try {
-			const orders = await Order.find();
-			res.status(200).json(orders);
-		} catch (err) {
-			res.status(500).json(err);
-		}
-	}
+  if (method === "GET") {
+    try {
+      const orders = await Order.find();
+      res.status(200).json(orders);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 
-	if (method === "POST") {
-		try {
-			const order = await Order.create(req.body);
-			res.status(201).json(order);
-		} catch (err) {
-			res.status(500).json(err);
-		}
-	}
+  if (method === "POST") {
+    try {
+      const order = await Order.create(req.body);
+      res.status(201).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 };
 
 export default handler;
